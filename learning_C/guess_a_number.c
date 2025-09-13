921952514ffe18ee34a10accb607d7993ffc96eb
Rahul-Git-Projects <rahul.sanapala2@gmail.com>
2025-09-13 11:13:05 +0530

    made some fixes to my code

--- learning_C/guess_a_number.c
+++ learning_C/guess_a_number.c
@@ -10,14 +10,14 @@ int main()
     int attempts_left = 5;
     int user_guess;
 
-    printf("This is a number guessing game\ni have chosen a number from 0 to 20 and you have totry to  guess it\nyou have 5 tries\nGood luck!");
+    printf("This is a number guessing game\nI have chosen a number from 0 to 20 and you have totry to  guess it\nyou have 5 tries\nGood luck!");
     while (attempts_left > 0)
     {
         printf("\nGuess the number: ");
         scanf("%d", &user_guess);
         while (user_guess < 0 || user_guess > 20)
         {
-            printf("\nYour guess is not in the range of the numbers i have mentioned please try again: ");
+            printf("\nYour guess is not in the range of the numbers I have mentioned please try again: ");
             scanf("%d", &user_guess);
         }
         if (user_guess < random_number)
@@ -35,6 +35,7 @@ int main()
         }
 
         --attempts_left;
-        printf("attempts left: %d", attempts_left);
+        printf("\nattempts left: %d", attempts_left);
     }
+    printf("\nYou lost!The number was %d\nBetter luck next time!", random_number);
 }
